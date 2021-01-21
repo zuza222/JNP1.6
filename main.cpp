f39dae67c3c225042e9f46655383c56df559f95b
Stanisław Świątkowski <sa.swiatkowski@student.uw.edu.pl>
2021-01-21 10:55:28 +0100

    W zasadzie działa

--- main.cpp
+++ main.cpp
@@ -1,70 +1,126 @@
 #include <iostream>
 
-int N = 100;
-int64_t tab[100];
-char *labels[100];
-
 //@TODO: To powinna być klasa abstrakcyjna
-//Nadklasa p-wartości
-class element_OOAsm {
-protected:
-    int64_t x;
-};
 
-//Nadklasa p-wartości
-class Rvalues : public virtual element_OOAsm {
+
+class Rvalues;  //Nadklasa p-wartości
+class Lvalues;  //Nadklasa l-wartości
+class num;
+class lea;
+class mem;
+
+class Processor {
+private:
+    size_t N;
+    int64_t* tab;
+    char **labels;
+
+    friend mem;
+    friend lea;
+
 public:
-    int64_t get_value() {return x;}
-};
+    Processor(size_t _N) : N(_N), tab(new int64_t[_N]), labels(new char*[_N]) {
+        //@TODO: Tych przypisań ma nie być w docelowym kodzie (służyły to testowania :-P)
+        labels[0] = "Papaj II";
+        labels[1] = "WTC";
 
-
-//@TODO: To też powinna być klasa abstrakcyjna
-//Nadklasa l-wartości
-class Lvalues : public virtual element_OOAsm {
-protected:
-    size_t address;
-    Lvalues(size_t addr) : address(addr) {x = tab[address];}
-public:
-    void set_address(size_t new_addr) {
-        address = new_addr;
-        x = tab[new_addr];
+        tab[1] = 911;
     }
+};
+
+//Nadklasa p-wartości
+class Rvalues { //: public virtual element_OOAsm {
+public:
+    virtual int64_t get_value(Processor *proc);
+};
+
+//@TODO: Ta klasa tylko ładnie wygląda, ale można ją usunąć XD
+class Lvalues {
+public:
+    virtual void set_value(Processor *proc, int64_t new_val);
 };
 
 class num : public Rvalues {
+private:
+    int64_t x;
 public:
     num(int64_t _x) {x = _x;}
-};
 
-class mem : public Rvalues, public Lvalues {
-public:
-    mem(Rvalues r) : Lvalues(r.get_value()) {}
+    int64_t get_value(Processor *proc) override;
 };
 
 class lea : public Rvalues {
+private:
+    char *id;
 public:
-    lea(char *id) {
-        for (size_t i = 0; i < N; ++i)
-            if (*labels[i] == *id) {
-                x = i;
-                break;
-            }
-    }
+    lea(char *_id) : id(_id) {}
+
+    int64_t get_value(Processor *proc) override;
 };
 
-void pomocnicza(Rvalues r) {
-    std::cout << r.get_value() << std::endl;
+class mem : public Rvalues, public Lvalues {
+
+private:
+    Rvalues *r;
+    mem(Rvalues *_r) : r(_r) {}
+
+public:
+    //@TODO: Te konstruktory są strasznie brzydkie, ale nie wiem jak zrobić referencję kiedy argumentem jest obiekt
+    mem(num n) : r(&n) {}
+    mem(lea l) : r(&l) {}
+    mem(mem &m) : r(&m) {}
+
+    int64_t get_value(Processor *proc);
+
+    void set_value(Processor *proc, int64_t new_val);
+};
+
+int64_t Rvalues::get_value(Processor *proc) {return 0;}
+
+void Lvalues::set_value(Processor *proc, int64_t new_val) {}
+
+int64_t num::get_value(Processor *proc) {return x;}
+
+int64_t lea::get_value(Processor *proc) {
+    for (size_t i = 0; i < proc->N; ++i) {
+        if (*id == *proc->labels[i])
+            return i;
+    }
+
+    //@TODO: Wyrzucamy jakiś wyjątek IDnotFound
+
+    return proc->N;
+}
+
+int64_t mem::get_value(Processor *proc) {
+    size_t address = r->get_value(proc);
+
+    if (address < proc->N) {
+        return proc->tab[address];
+    }
+    else {
+        //@TODO: Wyrzucamy jakiś wyjątek OutOfBoundsException
+    }
+}
+
+void mem::set_value(Processor *proc, int64_t new_val) {
+    size_t address = r->get_value(proc);
+    proc->tab[address] = new_val;
+}
+
+
+void pomocnicza(Processor *proc, Rvalues *r) {
+    std::cout << r->get_value(proc) << std::endl;
 }
 
 int main() {
-    tab[0] = 2137;
-    tab[1] = 911;
+    Processor proc(100);
 
-    labels[0] = "Papaj II";
-    labels[1] = "WTC";
+    mem m(num(0));
+    m.set_value(&proc, 2137);
 
-    lea q = lea("WTC");
-    mem m = mem(q);
+    mem m2(lea("WTC"));
 
-    pomocnicza(m);
+    pomocnicza(&proc, &m2);
+
 }
